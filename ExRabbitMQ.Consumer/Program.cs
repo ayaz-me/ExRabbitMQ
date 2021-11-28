aba5a56c7457e247c40d1514ee2fe88f0ea5fe98
Mehmet Ayaz <mehmet.ayaz@coresoft.com.tr>
2021-11-28 11:53:00 +0300

    first commit

---
+++ ExRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://kqrijscj:ekQcnDbu67XtUacb28LLNsRfjCdwFbQ0@roedeer.rmq.cloudamqp.com/kqrijscj");
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine($"Received: {message}");
+};
---
+++ ExRabbitMQ.Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://kqrijscj:ekQcnDbu67XtUacb28LLNsRfjCdwFbQ0@roedeer.rmq.cloudamqp.com/kqrijscj");
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "hello world!";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+for (int i = 0; i < 10; i++)
+{
+    channel.BasicPublish(String.Empty, "hello-queue", null, messageBody);
+    Console.WriteLine($"Sended: {message}");
+    Thread.Sleep(2000);
+}
